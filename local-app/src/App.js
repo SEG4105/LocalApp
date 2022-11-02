b55e8db2b88a55def65eaaa5918a644162e54198
skylathadani <skyla.thadani@gmail.com>
2022-11-02 12:02:38 -0400

    added image with azure front door, please use this url format to do work

--- local-app/src/App.js
+++ local-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://local-bzdwa6etgsggdngn.z01.azurefd.net/videos/Screen%20Shot%202022-10-26%20at%2012.08.10%20PM.png?sp=r&st=2022-11-02T15:58:15Z&se=2022-11-02T23:58:15Z&spr=https&sv=2021-06-08&sr=b&sig=pr5D6uMVCFxSZDxJAwUoFCyw0vOal9Ond3GXQUJYMHA%3D" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
