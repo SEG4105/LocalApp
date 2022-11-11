497d69c5cbb21f2c474f133df02d803099321992
Jack Clarke <jackfulcher09@gmail.com>
2022-11-11 12:58:20 -0500

    get started

--- local-app/src/App.js
+++ local-app/src/App.js
@@ -1,22 +1,97 @@
 import './App.css';
+import { BrowserRouter, Link, Route, Routes, Switch } from 'react-router-dom'
+import { Typography, Card, Button, Drawer, useScrollTrigger, Box, Toolbar, Divider, List, ListItem, ListItemButton } from '@mui/material';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player'
+import './video-react.css'
 
+function getBaseUrl() {
+  var re = new RegExp(/^.*\//);
+  document.getElementById('baseresult').innerHTML += re.exec(window.location.href);
+}
+
+function getRootUrl() {
+  document.getElementById('rootresult').innerHTML +=
+    window.location.origin
+      ? window.location.origin + '/'
+      : window.location.protocol + '/' + window.location.host + '/';
+
+  //
+  getBaseUrl();
+}
+
+const getVideos = () => {
+  return [
+    { video: "https://file-examples.com/storage/fe8c7eef0c6364f6c9504cc/2020/03/file_example_WEBM_480_900KB.webm", name: "World cooking", path: "/worldcooking" }
+  ]
+}
+
+const VideoDisplay = ({ video }) => (
+  <>
+    <Typography variant="h1">
+      {video.name}
+    </Typography>
+
+    <Box sx={{ maxWidth: "0.5vw" }}>
+      <ReactPlayer
+        url={video.video}
+      />
+    </Box>
+  </>
+)
+
+function VideoDrawer({ videos }) {
+  console.log(videos)
+
+  return (<Drawer variant="permanent">
+    <Toolbar>
+    </Toolbar>
+    <List>
+      <ListItem>
+        <Link to="/">
+          <ListItemButton>Home </ListItemButton>
+        </Link>
+      </ListItem>
+      {videos.map((video, index) => {
+        return (<div key={index}>
+          <Link to={video.path}>
+            <ListItemButton>{video.name} </ListItemButton>
+          </Link>
+          <Divider></Divider>
+        </div>)
+      })}
+    </List>
+  </Drawer>)
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://local-bzdwa6etgsggdngn.z01.azurefd.net/videos/Screen%20Shot%202022-10-26%20at%2012.08.10%20PM.png?sp=r&st=2022-11-02T15:58:15Z&se=2022-11-02T23:58:15Z&spr=https&sv=2021-06-08&sr=b&sig=pr5D6uMVCFxSZDxJAwUoFCyw0vOal9Ond3GXQUJYMHA%3D" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <VideoDrawer videos={getVideos()}></VideoDrawer>
+        <Routes>
+          <Route path='/' element={
+            <Typography variant="h1"> Welcome to the Cokeing Video Website</Typography>
+          }>
+          </Route>
+          <>
+            {
+              getVideos().map((video, index) => (
+                <Route key={index} path={"/worldcooking"} element={
+                  <>
+                    <VideoDisplay video={video}>
+                    </VideoDisplay>
+                  </>
+                } >
+                </Route>
+              ))
+            }
+          </>
+        </Routes>
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
