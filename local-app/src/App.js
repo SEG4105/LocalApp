69f28b355e05d26d404f5ff71692d418e65264f9
Skyla Thadani <40873016+skylathadani@users.noreply.github.com>
2022-11-18 14:20:01 -0500

    Update App.js

--- local-app/src/App.js
+++ local-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://local-bzdwa6etgsggdngn.z01.azurefd.net/videos/Screen%20Shot%202022-10-26%20at%2012.08.10%20PM.png?sp=r&st=2022-11-02T15:58:15Z&se=2022-11-02T23:58:15Z&spr=https&sv=2021-06-08&sr=b&sig=pr5D6uMVCFxSZDxJAwUoFCyw0vOal9Ond3GXQUJYMHA%3D" alt="logo" />
+        <img src="https://local-bzdwa6etgsggdngn.z01.azurefd.net/videos/Screen%20Shot%202022-10-26%20at%2012.08.10%20PM.png?sp=r&st=2022-11-18T19:17:57Z&se=2023-01-02T03:17:57Z&spr=https&sv=2021-06-08&sr=b&sig=2PhCvMmHrQmz1Ov6kOCW%2FKDHe6yxpLTUXuNAHKKJ0ug%3D" alt="logo" />  
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
